c5e65ab741d4c2d4e40ffc6369586fb64548942a
nm5578 <158161329+nm5578@users.noreply.github.com>
2024-04-20 16:18:43 -0400

    Update flatbigintadd.c fix

--- flatbigintadd.c
+++ flatbigintadd.c
@@ -28,34 +28,37 @@ int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
    lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
 
    /* Clear oSum's array if necessary. */
-   if (oSum->lLength > lSumLength)
+   if (oSum->lLength <= lSumLength) goto endif2;
       memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
-
+   endif2:
    /* Perform the addition. */
    ulCarry = 0;
    lIndex = 0;
    loop1:
+      if(lIndex >= lSumLength) goto endloop1;
       ulSum = ulCarry;
       ulCarry = 0;
       ulSum += oAddend1->aulDigits[lIndex];
-      if(ulSum < oAddend1->aulDigits[lIndex]) /* Check for overflow. */
+      if(ulSum >= oAddend1->aulDigits[lIndex]) goto endif3; /* Check for overflow. */
          ulCarry = 1;
+      endif3:
       ulSum += oAddend2->aulDigits[lIndex];
-      if (ulSum < oAddend2->aulDigits[lIndex]) /* Check for overflow. */
+      if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif4; /* Check for overflow. */
          ulCarry = 1;
+      endif4:
       oSum->aulDigits[lIndex] = ulSum;
-      if(lIndex >= lSumLength) goto endloop1;
       lIndex = lIndex + 1;
+      goto loop1;
    endloop1:
 
    /* Check for a carry out of the last "column" of the addition. */
-   if(ulCarry != 1) goto endif2;
-   if(lSumLength != MAX_DIGITS) goto endif3;
+   if(ulCarry != 1) goto endif5;
+   if(lSumLength != MAX_DIGITS) goto endif6;
    return FALSE;
-   endif3:
+   endif6:
    oSum -> aulDigits[lSumLength] = 1;
    lSumLength++;
-   endif2:
+   endif5:
    /* Set the length of the sum. */
    oSum->lLength = lSumLength;
 
