9f2c83064e68f1d27313ad344441a58d2c4ab5b4
Katherine Ren <kr5379@princeton.edu>
2024-04-19 20:42:28 -0400

    zfghdjk.

--- mywc.s
+++ mywc.s
@@ -39,14 +39,16 @@ main:
     loop1:
         // if ((iChar = getchar()) == EOF) go to endloop1;
         bl getchar
-        adr x0, iChar
-        ldr x0, [x0]
-        cmp x0, EOF
+        adr x1, iChar
+        ldr w0, [x1]
+        cmp w0, EOF
         blt endloop1
         
         // lCharCount++;
-        ldr w0, [sp, lCharCount]
-        add w0, w0, 1
+        adr x0, lCharCount
+        ldr x2, [x0]
+        add x2, x2, 1
+        str x2, [x0]
 
         // if (!isspace(iChar)) goto else1;
         adr x0, iChar
@@ -56,60 +58,74 @@ main:
         beq else1
 
         //if (!iInWord) goto endif2;
-        ldr x0, iInWord
-        cmp x0, FALSE
+        adr x3, iInWord
+        ldr w3, [x3]
+        cmp w3, FALSE
         beq endif2
 
         //lWordCount++;
-        ldr w0, [sp, lWordCount]
-        add w0, w0, 1
+        adr x4, lWordCount
+        ldr x2, [x4]
+        add x2, x2, 1
+        str x2, [x4]
 
         //iInWord = FALSE;
-        ldr w0, [sp, iInWord]
-        mov w0, FALSE
+        adr x2, iInWord
+        mov w3, FALSE
+        str w3, [x2]
+
         endif2:
+
         // goto endif1
         b endif1
 
         else1:
         //if (iInWord) goto endif3;
-        ldr x0, iInWord
-        cmp x0, TRUE
+        adr x1, iInWord
+        ldr w2, [x1]
+        cmp w2, TRUE
         beq endif3
-
+        
         //iInWord = TRUE;
-        ldr w0, [sp, iInWord]
-        mov w0, TRUE
+        mov w2, TRUE
+        str w2, [x1]
 
         endif3:
 
         endif1:
         //if (iChar != '\n') goto endif4;
         adr x0, iChar
-        ldr x0, [x0]
-        cmp x0, newline
+        ldr w1, [x0]
+        cmp w1, newline
         bne endif4
 
         //lLineCount++;
-        ldr w0, [sp, lLineCount]
-        add w0, w0, 1
+        adr x1, lLineCount
+        ldr x0, [x1]
+        add x0, x0, 1
+        str x0, [x1]
+
         endif4:
+
         b loop1
+
     endloop1:
 
     //if (!iInWord) goto endif5;
-    ldr x0, iInWord
-    cmp x0, FALSE
+    adr x1, iInWord
+    ldr w0, [x1]
+    cmp w0, FALSE
     beq endif5
 
     //lWordCount++;
-    ldr w0, [sp, lWordCount]
-    add w0, w0, 1
+    adr x0, lWordCount
+    ldr x1, [x0]
+    add x1, x1, 1
+    str x1, [x0]
     endif5:
 
     //printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
     adr x0, printfFormatStr
-    ldr x0, [x0]
     adr x1, lLineCount
     ldr x1, [x1]
     adr x2, lWordCount
