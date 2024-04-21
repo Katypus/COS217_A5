b4673440a094887cc6fb31a5e54c735ade3e2e7c
Katherine Ren <kr5379@princeton.edu>
2024-04-21 19:24:27 -0400

    r;jiro

--- bigintadd.s
+++ bigintadd.s
@@ -12,11 +12,14 @@ printfFormatStr:
 //    .quad '\n'
 //----------------------------------------
 
-.equ MAIN_STACK_BYTECOUNT, 32
+.equ LARGER_STACK_BYTECOUNT, 32
+.equ ADD_STACK_BYTECOUNT, 56
+.equ ZERO, 0
+.equ UNSIGNED_LONG_SIZE, 8
 
 bigintlarger:
     // Prolog
-    sub sp, sp, MAIN_STACK_BYTECOUNT
+    sub sp, sp, LARGER_STACK_BYTECOUNT
     str x30, [sp]
 
     // Save lLength1
@@ -46,3 +49,55 @@ bigintlarger:
 
     endif1:
     ret
+
+bigintadd:
+    // Prolog
+    sub sp, sp, ADD_STACK_BYTECOUNT
+    str x30, [sp]
+
+    // Save oAddend1
+    str x0, [sp, 8]
+    // Save oAddend2
+    str x1, [sp, 16]
+    // Save oSum
+    str x2, [sp, 24]
+
+    // Load parameters
+    ldr x0, [sp, 8]
+    ldr x1, [sp, 16]
+    ldr x2, [sp, 24]
+
+    // lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
+    bl bigintlarger
+    str x0, [sp, 32]
+    
+    // Load oSum
+    ldr x0, [sp, 24]
+    ldr x1, [sp, 32]
+
+    cmp x0, x1
+    ble endif2
+
+    // memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+    ldr x0, [sp, 24]
+    add x0, x0, 8
+    ldr x1, ZERO
+    ldr x2, MAX_DIGITS
+    mul x2, x2, UNSIGNED_LONG_SIZE
+    bl memset
+
+    endif2:
+    // ulCarry = 0;
+    ldr x0, ZERO
+    str x0, [sp, 40]
+
+    // lIndex = 0
+    ldr x1, ZERO
+    str x2, [sp, 48]
+
+    loop1:
+        
+
+
+
+
