03c57db9dd69f388b5b56d6f4a343f81cdae2a5d
Katherine Ren <kr5379@princeton.edu>
2024-04-21 18:43:51 -0400

    lin

---
+++ bigintadd.s
@@ -0,0 +1,48 @@
+//-------------------------------
+// bigintadd.s
+// Authors: Nadja and Kat
+//----------------------------------------
+    .equ FALSE, 0
+    .equ TRUE, 1
+//----------------------------------------
+    .section .rodata
+printfFormatStr:
+    .string "%7ld %7ld %7ld\n"
+//newline:
+//    .quad '\n'
+//----------------------------------------
+
+.equ MAIN_STACK_BYTECOUNT, 32
+
+bigintlarger:
+    // Prolog
+    sub sp, sp, MAIN_STACK_BYTECOUNT
+    str x30, [sp]
+
+    // Save lLength1
+    str x0, [sp, 16]
+    // Save lLength2
+    str x1, [sp, 8]
+
+    // load lLength1
+    ldr x0, [sp, 16]
+    // load lLength2
+    ldr x1, [sp, 8]
+
+    // if (lLength1 <= lLength2) goto else1;
+    cmp x0, x1
+    ble goto else1
+
+    // lLarger = lLength1;
+    // no change required
+    b endif1
+
+    else1:
+    str x0, [sp, 8]
+    ldr x0, [sp, 8]
+
+    ldr x30, [sp]
+    add sp, sp, 16
+    
+    endif1:
+    ret
