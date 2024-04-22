9ef812144c79d4a9ad5b96eae7cc8de117168f00
Katherine Ren <kr5379@princeton.edu>
2024-04-21 20:33:10 -0400

    efhled

--- bigintadd.s
+++ bigintadd.s
@@ -97,7 +97,104 @@ bigintadd:
     str x2, [sp, 48]
 
     loop1:
+        // if(lIndex >= lSumLength) goto endloop1;
+        ldr x0, [sp, 48]
+        ldr x1, [sp, 32]
+        cmp x0, x1
+        bge endloop1
 
+        // ulSum = ulCarry;
+        ldr x0, [sp, 40]
+        str x0, [sp, 56]
+
+        // ulCarry = 0;
+        ldr x0, ZERO
+        str x0, [sp, 40]
+
+        // ulSum += oAddend1->aulDigits[lIndex];
+        ldr x0, [sp, 8]
+        add x0, x0, 8
+        ldr x1, [sp, 48]
+        mov x2, x1
+        ldr x0, [x0, x2, lsl 3]
+        ldr x3, [sp, 56]
+        add x3, x3, x0
+        str x3, [sp, 56]
+
+        cmp x3, x0
+        bge endif3
+
+        // ulCarry = 1;
+        ldr x4, 1
+        str x4, [sp, 40]
+
+        endif3:
+        // ulSum += oAddend2->aulDigits[lIndex];
+        ldr x0, [sp, 16]
+        add x0, x0, 8
+        ldr x1, [sp, 48]
+        mov x2, x1
+        ldr x0, [x0, x2, lsl 3]
+        ldr x3, [sp, 56]
+        add x3, x3, x0
+        str x3, [sp, 56]
+
+        cmp x3, x0
+        bge endif4
+
+        // ulCarry = 1;
+        ldr x4, 1
+        str x4, [sp, 40]
+
+        endif4:
+        // oSum->aulDigits[lIndex] = ulSum;
+        ldr x0, [sp, 24]
+        add x0, x0, 8
+        ldr x1, [sp, 48]
+        ldr x3, [sp, 56]
+        str x3, [x0, x1, lsl 3]
+
+        // lIndex = lIndex + 1;
+        ldr x0, [sp, 48]
+        add x0, x0, 1
+        str x0, [sp, 48]
+
+        b loop1
+
+    endloop1:
+        // if(ulCarry != 1) goto endif5;
+        ldr x0, [sp, 40]
+        cmp x0, 1
+        bne endif5
+
+        //if(lSumLength != MAX_DIGITS) goto endif6;
+        ldr x0, [sp, 32]
+        ldr x1, MAX_DIGITS
+        cmp x0, x1
+        bne endif6
+        ldr x0, FALSE
+
+        endif6:
+        // oSum -> aulDigits[lSumLength] = 1;
+        ldr x0, [sp, 24]
+        add x0, x0, 8
+        ldr x1, [sp, 32]
+        ldr x3, 1
+        str x3, [x0, x1, lsl 3]
+
+        // lSumLength++;
+        add x1, x1, 1
+        str x1, [sp, 32]
+
+        endif5:
+        //oSum->lLength = lSumLength;
+        ldr x0, [sp, 24]
+        ldr x1, [sp, 32]
+        str x1, [x0, 0, lsl 3]
+
+        ldr x0, TRUE
+
+        ret
 
 
 
