1921831db4ac070712e370bed7193e1e85fd8795
Katherine Ren <kr5379@princeton.edu>
2024-04-23 22:24:59 -0400

    ldr first??

--- bigintadd.s
+++ bigintadd.s
@@ -103,14 +103,16 @@ BigInt_add:
 
     endif2:
     // ulCarry = 0;
+    ldr x0, [sp, ULCARRY]
     mov x0, 0
     str x0, [sp, ULCARRY]
-    ldr x0, [sp, ULCARRY]
+    
 
     // lIndex = 0
+    ldr x1, [sp, LINDEX]
     mov x1, 0
     str x1, [sp, LINDEX]
-    ldr x1, [sp, LINDEX]
+    
 
     loop1:
         // if(lIndex >= lSumLength) goto endloop1;
